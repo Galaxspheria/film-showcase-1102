943883759b702d1701ad1cad80067ca903f93fb1
Hunter Hancock <hunterghancock@gmail.com>
2019-03-01 15:50:01 -0500

    Basic Google Sheets/Drive CDN setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Tabletop from 'tabletop';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    var that = this
+    Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/14i0nvudUdYZkTer6euXjCnceQKkxGRq5dStdpHlPUn4/edit?usp=sharing',
+      callback: function(data) { 
+          console.log(data)
+          that.setState({data})
+      },
+      simpleSheet: true } )
+  }
+
+  driveURLConvert(url) {
+    return "https://docs.google.com/uc?id=" + url.substring(32, 65)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.state.data?
+            this.state.data.map((d) => (
+              <p>
+                {d.Film}
+                {d.Image?
+                  <img src={this.driveURLConvert(d.Image)}></img>
+                :null}
+              </p>
+            ))
+          :<div className="loader"></div>}
         </header>
       </div>
     );
