070be7d8048ab082c057bb6324cadf7a665e0479
Hunter Hancock <hunterghancock@gmail.com>
2019-03-08 12:57:12 -0500

    Header basics

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import '../styles/Header.scss';
 
 class Header extends Component {
     render() {
         return (
             <div className="Header fade-down">
-                Header
+                <Link to="/"><img src="https://via.placeholder.com/50"/></Link>
+                <div style={{textAlign: 'right', flexGrow: 1}}>
+                    <NavLink className="header-button" activeClassName="hb-active" exact to="/">Home</NavLink>
+                    <NavLink className="header-button" activeClassName="hb-active" to="/authors">Authors</NavLink>
+                    <NavLink className="header-button" activeClassName="hb-active" to="/about">About</NavLink>
+                </div>
             </div>
         );
     }
