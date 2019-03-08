347a8adc17ca1e6af5483355f524893b20f44bef
devansh2001 <dponda3@gatech.edu>
2019-03-07 22:25:30 -0500

    Author page: everything except images works

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Author from './components/Author';
 class App extends Component {
   constructor(props) {
     super(props)
-    
+
     this.state = {
       data: null
     }
--- src/components/Author.js
+++ src/components/Author.js
@@ -1,63 +1,65 @@
 import React, { Component } from 'react';
 import '../styles/Author.css'
 
-className Author extends Component {
-  const author;
+class Author extends Component {
     constructor(props) {
         super(props)
         this.state = {
             id: this.props.match.params.id
         }
-        author = this.props.data.author.elements;
-
     }
 
     render() {
+      const author = this.props.data.author.elements[this.state.id]
         return (
+          <div>
           <div className='author-data'>
             <div className='author-image'>
-              <img id = 'image-auth'src={author['Portrait/Headshot Link']}>
-                //replace src to {author.PortraitLink}
-              </img>
+              <img id = 'image-auth'src={author['Portrait/Headshot Link']}/>
             </div>
             <div className='author-detail'>
-              <p id='name-author'>{author['Author']}</p>
-              <p className='author-subdata'>Dates Alive: {author['Birth Year']} - {author['Death Year']}</p>
+              <p id='name-author'>
+                {author['Author']}
+              </p>
+              <p className='author-subdata'>
+                Dates Alive: {author['Birth Year']} - {author['Death Year']}
+              </p>
               <p className='author-subdata'>Important Works:
                 {author['Important Work 1 Name']},
-              {author['Important Work 2 Name']},
+                {author['Important Work 2 Name']},
                 {author['Important Work 3 Name']}
               </p>
               <p className='author-subdata'>Adapted Works:
                 {author['Adapted Work 1 Name']},
                 {author['Adapted Work 2 Name']},
-                {author['Adapted Work 3 Name']}</p>
+                {author['Adapted Work 3 Name']}
+              </p>
             </div>
           </div>
           <div className='adapted-works'>
-            <p className='direct-head' style='text-align: center'>Adapted Works</p>
+            <p className='direct-head' style={{textAlign: 'center'}}>Adapted Works</p>
             <div className='adapted'>
-              <img src="author['Portrait/Headshot Link']" alt="Picture">
+              <img src={author['Portrait/Headshot Link']} alt="Picture"></img>
               <p>Movie Name: {author['Adapted Work 1 Name']}</p>
               <p>Author's Role: {author['Role in Adapted Work 1']}</p>
             </div>
             <div className='adapted'>
-              <img src={author['Portrait/Headshot Link']} alt="Picture">
+              <img src={author['Portrait/Headshot Link']} alt="Picture"></img>
               <p>Movie Name: {author['Adapted Work 2 Name']}</p>
               <p>Author's Role: {author['Role in Adapted Work 2']}</p>
             </div>
             <div className='adapted'>
-              <img src={author['Portrait/Headshot Link']} alt="Picture">
+              <img src={author['Portrait/Headshot Link']} alt="Picture"></img>
               <p>Movie Name: {author['Adapted Work 3 Name']}</p>
               <p>Author's Role: {author['Role in Adapted Work 3']}</p>
             </div>
           </div>
 
           <div className='impact-sect'>
-            <p className='direct-head' style='text-align: center'>Impact Description</p>
+            <p className='direct-head' style={{textAlign: 'center'}}>Impact Description</p>
             <p>{author['Impact']}</p>
           </div>
-
+          </div>
         );
     }
 }
