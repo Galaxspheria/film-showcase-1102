274128e80de1a7277432e94353a7c2b1461af092
devansh2001 <dponda3@gatech.edu>
2019-03-07 21:59:01 -0500

    Corrected images

--- src/components/Author.js
+++ src/components/Author.js
@@ -16,12 +16,12 @@ className Author extends Component {
         return (
           <div className='author-data'>
             <div className='author-image'>
-              <img id = 'image-auth'src='../../public/image.jpg'>
+              <img id = 'image-auth'src={author['Portrait/Headshot Link']}>
                 //replace src to {author.PortraitLink}
               </img>
             </div>
             <div className='author-detail'>
-              <p id='name-author'>{author.Author}</p>
+              <p id='name-author'>{author['Author']}</p>
               <p className='author-subdata'>Dates Alive: {author['Birth Year']} - {author['Death Year']}</p>
               <p className='author-subdata'>Important Works:
                 {author['Important Work 1 Name']},
@@ -37,17 +37,17 @@ className Author extends Component {
           <div className='adapted-works'>
             <p className='direct-head' style='text-align: center'>Adapted Works</p>
             <div className='adapted'>
-              <img src="../../public/image.jpg" alt="Picture">
+              <img src="author['Portrait/Headshot Link']" alt="Picture">
               <p>Movie Name: {author['Adapted Work 1 Name']}</p>
               <p>Author's Role: {author['Role in Adapted Work 1']}</p>
             </div>
             <div className='adapted'>
-              <img src="../../public/image.jpg" alt="Picture">
+              <img src={author['Portrait/Headshot Link']} alt="Picture">
               <p>Movie Name: {author['Adapted Work 2 Name']}</p>
               <p>Author's Role: {author['Role in Adapted Work 2']}</p>
             </div>
             <div className='adapted'>
-              <img src="../../public/image.jpg" alt="Picture">
+              <img src={author['Portrait/Headshot Link']} alt="Picture">
               <p>Movie Name: {author['Adapted Work 3 Name']}</p>
               <p>Author's Role: {author['Role in Adapted Work 3']}</p>
             </div>
@@ -55,7 +55,7 @@ className Author extends Component {
 
           <div className='impact-sect'>
             <p className='direct-head' style='text-align: center'>Impact Description</p>
-            <p>{author.Impact}</p>
+            <p>{author['Impact']}</p>
           </div>
 
         );
