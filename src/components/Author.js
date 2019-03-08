b478bbd9665d797bf41561ebacbaee329dcf11ff
devansh2001 <dponda3@gatech.edu>
2019-03-07 21:57:09 -0500

    Added integration to GoogleSheets

--- src/components/Author.js
+++ src/components/Author.js
@@ -22,34 +22,34 @@ className Author extends Component {
             </div>
             <div className='author-detail'>
               <p id='name-author'>{author.Author}</p>
-              <p className='author-subdata'>Dates Alive: {author.BirthYear} - {author.DeathYear}</p>
+              <p className='author-subdata'>Dates Alive: {author['Birth Year']} - {author['Death Year']}</p>
               <p className='author-subdata'>Important Works:
-                {author.ImportantWork1Name},
-                {author.ImportantWork2Name},
-                {author.ImportantWork2Name}
+                {author['Important Work 1 Name']},
+              {author['Important Work 2 Name']},
+                {author['Important Work 3 Name']}
               </p>
-              <p className='author-subdata'>Adapted Works: {author.AdaptedWork1},
-                {author.AdaptedWork2},
-                {author.AdaptedWork3}</p>
+              <p className='author-subdata'>Adapted Works:
+                {author['Adapted Work 1 Name']},
+                {author['Adapted Work 2 Name']},
+                {author['Adapted Work 3 Name']}</p>
             </div>
           </div>
-
           <div className='adapted-works'>
             <p className='direct-head' style='text-align: center'>Adapted Works</p>
             <div className='adapted'>
               <img src="../../public/image.jpg" alt="Picture">
-              <p>Movie Name:</p>
-              <p>Author's Role:</p>
+              <p>Movie Name: {author['Adapted Work 1 Name']}</p>
+              <p>Author's Role: {author['Role in Adapted Work 1']}</p>
             </div>
             <div className='adapted'>
               <img src="../../public/image.jpg" alt="Picture">
-              <p>Movie Name:</p>
-              <p>Author's Role:</p>
+              <p>Movie Name: {author['Adapted Work 2 Name']}</p>
+              <p>Author's Role: {author['Role in Adapted Work 2']}</p>
             </div>
             <div className='adapted'>
               <img src="../../public/image.jpg" alt="Picture">
-              <p>Movie Name:</p>
-              <p>Author's Role:</p>
+              <p>Movie Name: {author['Adapted Work 3 Name']}</p>
+              <p>Author's Role: {author['Role in Adapted Work 3']}</p>
             </div>
           </div>
 
