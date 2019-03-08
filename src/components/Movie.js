d1e882d282fb9b49a23f7b4ec6f4d219ac136372
Simar Kareer <skareer@outlook.com>
2019-03-07 20:23:52 -0500

    added Movie.js

---
+++ src/components/Movie.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        {this.props.data.film.elements.map((d, i) => (
+            <p key={i}>
+                {d.Film}
+                {d.Image?
+                    <img alt={d.Film} src={d.Image}></img>
+                :null}
+            </p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Home;
